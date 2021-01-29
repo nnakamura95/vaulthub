cfa7ecea0f3f9ba24a6b28e1ffc503b299d8b460
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2021-01-29 14:53:39 +0900

    Init commit

---
+++ src/main/java/us/nln/vaulthub/VaulthubApplication.java
@@ -0,0 +1,13 @@
+package us.nln.vaulthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaulthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaulthubApplication.class, args);
+	}
+
+}
